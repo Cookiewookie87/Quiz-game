ce77e8b5bd3fd47fe9d7ed1c9eadd47e7b154aee
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-03-21 16:05:32 +0100

    Added exercise for quiz on better way

--- script.js
+++ script.js
@@ -21,7 +21,17 @@ c) correct answer (I would use a number for this)
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
-*/
+
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+
 
 var Question = function(question, answers, correctAnswer){ // function constructor
     this.question = question;
@@ -59,6 +69,93 @@ Question.prototype.randQuest = function() { // method for displaying question an
     }
 }
 
-
-
 console.log(q1.randQuest()); // Zakaj to dela??
+*/
+(function () {
+    var Question = function(question, answers, correctAnswer) { // function constructor
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    };
+    // creating questions
+    var q1 = new Question("Are you over 18 years old?", ["Yes", "No"], 1);
+    var q2 = new Question("Are you from Ljubljana?", ["Yes", "No"], 2);
+    var q3 = new Question("What frameworks do you use?", ["Angular", "Vue", "React", "None"], 4);
+    var q4 = new Question("Do you hate CSGO?", ["Yes", "No", "Not sure"], 3);
+    // storing them in array
+    var quizArr = [q1, q2, q3, q4];
+
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log((i+1) + ". " + this.answers[i]);
+        }
+    };
+
+    Question.prototype.checkAnswer = function(userAnswer) {
+        if (userAnswer === this.correctAnswer) {
+            console.log("Your answer is CORRECT!!");
+        } else {
+            console.log("Your answer is wrong :(");
+        }
+    };
+
+    startQuiz();
+
+    function startQuiz() {
+        var rand = Math.floor(Math.random() * quizArr.length);
+        quizArr[rand].displayQuestion();
+        var userAnswer = parseInt(prompt("Please enter answer number:"));
+        quizArr[rand].checkAnswer(userAnswer);
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
